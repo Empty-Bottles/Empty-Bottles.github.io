a27d301d77ef53b5c78ac38ab5f5981a3671cf4e
Empty-Bottles <107383986+Empty-Bottles@users.noreply.github.com>
2023-01-09 02:52:03 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-
+let clock = document.getElementById("clock");
+function getClock(){
+  const date = new Date();
+  clock.innerText += " "+String(date.getHours()).padStart(2,"0")+" : "+String(date.getMinutes()).padStart(2,"0")+":"+String(date.getSeconds()).padStart(2,"0");
+}
+setInterval(getClock, 1000);
