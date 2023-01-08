902bfdb7fa905e356f3a133015e1fbe9a7ecca16
Empty-Bottles <107383986+Empty-Bottles@users.noreply.github.com>
2023-01-09 03:07:37 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let clock = document.getElementById("clock");
 function getClock(){
   const date = new Date();
-  clock.innerText = "The time is "+String(date.getHours()).padStart(2,"0")+" : "+String(date.getMinutes()).padStart(2,"0")+":"+String(date.getSeconds()).padStart(2,"0");
+  clock.innerText = "The time is "+String(date.getHours()).padStart(2,"0")+" : "+String(date.getMinutes()).padStart(2,"0")+" : "+String(date.getSeconds()).padStart(2,"0");
 }
 setInterval(getClock, 1000);
